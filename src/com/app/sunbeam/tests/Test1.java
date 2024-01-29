2eaa1f11c8bbc371768231f2450823bb53319a68
kamit1905 <73192278+kamit1905@users.noreply.github.com>
2024-01-29 18:18:56 +0530

    Update Test1.java

--- src/com/app/sunbeam/tests/Test1.java
+++ src/com/app/sunbeam/tests/Test1.java
@@ -20,14 +20,14 @@ public class Test1 extends BaseTest {
 	@Test(enabled = true ,priority = 3)
 	public void sample2() {
 		Assert.assertEquals("kishor", "kishor");
-		Assert.assertFalse(true);
+		//Assert.assertFalse(true);
 	}
 	
 	@Test(enabled=true,priority = 4)
 	public void LoginTest() {
 		LoginPage login = new LoginPage();
 		login.EnterUserName("amit789@gmail.com");
-		Assert.assertFalse(false);
+		Assert.assertFalse(true);
 		login.ClickOnNextButton();
 	}
 
