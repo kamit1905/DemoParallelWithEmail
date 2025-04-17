52889ebd0832679523558579ffa4eaca56df54d2
kamit1905 <73192278+kamit1905@users.noreply.github.com>
2025-04-17 16:05:36 +0530

    Update Driver.java

--- src/com/app/sunbeam/Driver/Driver.java
+++ src/com/app/sunbeam/Driver/Driver.java
@@ -6,12 +6,10 @@ import com.app.sunbeam.Configurations.PropertyReader;
 
 public class Driver {
 	
-	public static WebDriver driver;
-	
 	public static void initDriver() {
 		String browser= new PropertyReader().getBrowserName();
 		String url = new PropertyReader().getUrl();
-		driver = DriverFactory.getDriver(browser);
+		WebDriver driver = DriverFactory.getDriver(browser);
 		DriverManager.setDriver(driver);
 		DriverManager.getDriver().manage().window().maximize();
 		DriverManager.getDriver().get(url);
